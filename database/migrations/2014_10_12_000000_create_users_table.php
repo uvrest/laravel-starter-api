e2b150b8c344b3c203b12c0f807543875b2e6efb
Uverest <mailtogabrielcoelho@gmail.com>
2023-08-14 10:19:46 -0300

    Added soft delete support for users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('account_type');
+            $table->string('avatar_thumbnail')->nullable();
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
